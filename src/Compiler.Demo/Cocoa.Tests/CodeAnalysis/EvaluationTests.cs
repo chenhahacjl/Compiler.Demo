cccaa700d37dd62ac2a6b28091cbda114b7169e8
Cmile_96 <473436776@qq.com>
2021-10-02 16:12:35 +0800

    增加 Evaluation 测试
    增加 Evaluation 测试

---
+++ src/Compiler.Demo/Cocoa.Tests/CodeAnalysis/EvaluationTests.cs
@@ -0,0 +1,46 @@
+﻿using Cocoa.CodeAnalysis;
+using Cocoa.CodeAnalysis.Syntax;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Cocoa.Tests.CodeAnalysis
+{
+    public class EvaluationTests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("+1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("12 + 34", 46)]
+        [InlineData("12 - 3", 9)]
+        [InlineData("4 * 2", 8)]
+        [InlineData("9 / 3", 3)]
+        [InlineData("(10)", 10)]
+        [InlineData("12 == 11", false)]
+        [InlineData("3 == 3", true)]
+        [InlineData("12 != 11", true)]
+        [InlineData("3 != 3", false)]
+        [InlineData("false == false", true)]
+        [InlineData("true == false", false)]
+        [InlineData("false != false", false)]
+        [InlineData("true != false", true)]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        [InlineData("(a = 10) * a", 100)]
+        public void SyntacFact_GetText_RoundTrips(string text, object expectedResult)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+            var compilation = new Compilation(syntaxTree);
+            var variable = new Dictionary<VariableSymbol, object>();
+            var result = compilation.Evaluate(variable);
+
+            Assert.Empty(result.Diagnostics);
+            Assert.Equal(expectedResult, result.Value);
+        }
+    }
+}
