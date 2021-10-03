e8c814804bf87b30b1d03cef58da24e7593b64fb
Cmile_96 <473436776@qq.com>
2021-10-03 20:56:33 +0800

    增加 SourceText 测试
    增加 SourceText 测试

---
+++ src/Compiler.Demo/Cocoa.Tests/CodeAnalysis/Text/SourceTextTests.cs
@@ -0,0 +1,18 @@
+﻿using Cocoa.CodeAnalysis.Text;
+using Xunit;
+
+namespace Cocoa.Tests.CodeAnalysis.Text
+{
+    public class SourceTextTests
+    {
+        [Theory]
+        [InlineData(".", 1)]
+        [InlineData(".\r\n", 2)]
+        [InlineData(".\r\n\r\n", 3)]
+        public void SourceText_IncludeLastLine(string text, int expectedLineCount)
+        {
+            var sourceText = SourceText.From(text);
+            Assert.Equal(expectedLineCount, sourceText.Lines.Length);
+        }
+    }
+}
