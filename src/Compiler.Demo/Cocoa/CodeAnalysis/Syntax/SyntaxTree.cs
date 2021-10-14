88fd91dc83115b7b57cd16679f222e192ffea2d4
Cmile_96 <473436776@qq.com>
2021-10-14 15:14:15 +0800

    更新一些零散东西
    更新一些零散东西

--- src/Compiler.Demo/Cocoa/CodeAnalysis/Syntax/SyntaxTree.cs
+++ src/Compiler.Demo/Cocoa/CodeAnalysis/Syntax/SyntaxTree.cs
@@ -14,10 +14,9 @@ namespace Cocoa.CodeAnalysis.Syntax
         {
             var parser = new Parser(text);
             var root = parser.ParseCompilationUnit();
-            var diagnostics = parser.Diagnostics.ToImmutableArray();
 
             Text = text;
-            Diagnostics = diagnostics;
+            Diagnostics = parser.Diagnostics.ToImmutableArray();
             Root = root;
         }
 
