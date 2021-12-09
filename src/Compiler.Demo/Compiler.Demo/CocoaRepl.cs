dcd85e6b19e41892a96399758b4388d6b23b8c33
Cmile_96 <473436776@qq.com>
2021-12-09 21:24:27 +0800

    增加字符串高亮显示

--- src/Compiler.Demo/Compiler.Demo/CocoaRepl.cs
+++ src/Compiler.Demo/Compiler.Demo/CocoaRepl.cs
@@ -21,8 +21,9 @@ namespace Compiler.Demo
             foreach (var token in tokens)
             {
                 var isKeyword = token.Kind.ToString().EndsWith("Keyword");
-                var isNumber = token.Kind == SyntaxKind.NumberToken;
                 var isIdentifier = token.Kind == SyntaxKind.IdentifierToken;
+                var isNumber = token.Kind == SyntaxKind.NumberToken;
+                var isString = token.Kind == SyntaxKind.StringToken;
 
                 if (isKeyword)
                     Console.ForegroundColor = ConsoleColor.Blue;
@@ -30,6 +31,8 @@ namespace Compiler.Demo
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                 else if (isNumber)
                     Console.ForegroundColor = ConsoleColor.Cyan;
+                else if (isString)
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                 else
                     Console.ForegroundColor = ConsoleColor.DarkGray;
 
@@ -105,7 +108,7 @@ namespace Compiler.Demo
 
             if (!result.Diagnostics.Any())
             {
-                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine(result.Value);
                 Console.ResetColor();
 
