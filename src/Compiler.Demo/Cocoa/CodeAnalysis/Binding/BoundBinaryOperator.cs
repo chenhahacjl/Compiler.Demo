8309ad1f2a4b79af069fa0ec50f695c476b056e0
Cmile_96 <473436776@qq.com>
2021-12-10 13:49:39 +0800

    实现 字符串 != == 运算符 运算

--- src/Compiler.Demo/Cocoa/CodeAnalysis/Binding/BoundBinaryOperator.cs
+++ src/Compiler.Demo/Cocoa/CodeAnalysis/Binding/BoundBinaryOperator.cs
@@ -60,6 +60,8 @@ namespace Cocoa.CodeAnalysis.Binding
             new BoundBinaryOperator(SyntaxKind.BangEqualsToken, BoundBinaryOperatorKind.NotEquals, TypeSymbol.Boolean, TypeSymbol.Boolean),
 
             new BoundBinaryOperator(SyntaxKind.PlusToken, BoundBinaryOperatorKind.Addition, TypeSymbol.String),
+            new BoundBinaryOperator(SyntaxKind.EqualsEqualsToken, BoundBinaryOperatorKind.Equals, TypeSymbol.String, TypeSymbol.Boolean),
+            new BoundBinaryOperator(SyntaxKind.BangEqualsToken, BoundBinaryOperatorKind.NotEquals, TypeSymbol.String, TypeSymbol.Boolean),
         };
 
         public static BoundBinaryOperator Bind(SyntaxKind syntaxKind, TypeSymbol leftType, TypeSymbol rightType)
