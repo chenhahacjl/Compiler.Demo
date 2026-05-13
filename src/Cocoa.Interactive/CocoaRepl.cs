9ceafd6a77a6c4a640fa693cd60376ee272adc42
Cmile_96 <chenjiuliu@outlook.com>
2026-05-13 22:47:28 +0800

    修复EP16一些BUG

--- src/Cocoa.Interactive/CocoaRepl.cs
+++ src/Cocoa.Interactive/CocoaRepl.cs
@@ -9,259 +9,254 @@ using Cocoa.IO;
 
 namespace Cocoa.Interactive
 {
-	internal sealed class CocoaRepl : Repl
-	{
-		private bool _loadingSubmission;
-		private Compilation? _previous;
-		private bool _showTree;
-		private bool _showProgram;
-		private readonly Dictionary<VariableSymbol, object> _variables = new Dictionary<VariableSymbol, object>();
+    internal sealed class CocoaRepl : Repl
+    {
+        private bool _loadingSubmission;
+        private static readonly Compilation emptyCompilation = new Compilation();
+        private Compilation? _previous;
+        private bool _showTree;
+        private bool _showProgram;
+        private readonly Dictionary<VariableSymbol, object> _variables = new Dictionary<VariableSymbol, object>();
 
-		public CocoaRepl()
-		{
-			LoadSubmissions();
-		}
-
-		protected override void RenderLine(string line)
-		{
-			var tokens = SyntaxTree.ParseTokens(line);
-			foreach (var token in tokens)
-			{
-				var isKeyword = token.Kind.ToString().EndsWith("Keyword");
-				var isIdentifier = token.Kind == SyntaxKind.IdentifierToken;
-				var isNumber = token.Kind == SyntaxKind.NumberToken;
-				var isString = token.Kind == SyntaxKind.StringToken;
-
-				if (isKeyword)
-					Console.ForegroundColor = ConsoleColor.Blue;
-				else if (isIdentifier)
-					Console.ForegroundColor = ConsoleColor.DarkYellow;
-				else if (isNumber)
-					Console.ForegroundColor = ConsoleColor.Cyan;
-				else if (isString)
-					Console.ForegroundColor = ConsoleColor.Magenta;
-				else
-					Console.ForegroundColor = ConsoleColor.DarkGray;
-
-				Console.Write(token.Text);
-
-				Console.ResetColor();
-			}
-		}
-
-		[MetaCommand("cls", "Clears the screen")]
-		private void EvaluateCls()
-		{
-			Console.Clear();
-		}
-
-		[MetaCommand("reset", "Clears all previous submissions")]
-		private void EvaluateReset()
-		{
-			_previous = null;
-			_variables.Clear();
-
-			ClearSubmissions();
-		}
-
-		[MetaCommand("showTree", "Shows the parse tree")]
-		private void EvaluateShowTree()
-		{
-			_showTree = !_showTree;
-			Console.WriteLine(_showTree ? "Showing parse trees." : "Not showing parse trees.");
-		}
-
-		[MetaCommand("showProgram", "Shows the bound tree")]
-		private void EvaluateShowProgram()
-		{
-			_showProgram = !_showProgram;
-			Console.WriteLine(_showProgram ? "Showing bound trees." : "Not showing bound trees.");
-		}
-
-		[MetaCommand("load", "Loads a script file")]
-		private void EvaluateLoad(string path)
-		{
-			path = Path.GetFullPath(path);
-
-			if (!File.Exists(path))
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine($"error: file does not exist '{path}'");
-				Console.ResetColor();
-
-				return;
-			}
-
-			var text = File.ReadAllText(path);
-
-			EvaluateSubmission(text);
-		}
-
-		[MetaCommand("ls", "Lists all symbols")]
-		private void EvaluateLs()
-		{
-			if (_previous == null)
-			{
-				return;
-			}
-			
-			var symbols = _previous.GetSymbols().OrderBy(s => s.Kind).ThenBy(s => s.Name);
-			foreach (var symbol in symbols)
-			{
-				symbol.WriteTo(Console.Out);
-				Console.WriteLine();
-			}
-		}
-
-		[MetaCommand("dump", "Shows bound tree of a given function")]
-		private void EvaluateDump(string functionName)
+        public CocoaRepl()
         {
-            if (_previous == null)
+            LoadSubmissions();
+        }
+
+        protected override void RenderLine(string line)
+        {
+            var tokens = SyntaxTree.ParseTokens(line);
+            foreach (var token in tokens)
+            {
+                var isKeyword = token.Kind.ToString().EndsWith("Keyword");
+                var isIdentifier = token.Kind == SyntaxKind.IdentifierToken;
+                var isNumber = token.Kind == SyntaxKind.NumberToken;
+                var isString = token.Kind == SyntaxKind.StringToken;
+
+                if (isKeyword)
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else if (isIdentifier)
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                else if (isNumber)
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                else if (isString)
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                else
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                Console.Write(token.Text);
+
+                Console.ResetColor();
+            }
+        }
+
+        [MetaCommand("cls", "Clears the screen")]
+        private void EvaluateCls()
+        {
+            Console.Clear();
+        }
+
+        [MetaCommand("reset", "Clears all previous submissions")]
+        private void EvaluateReset()
+        {
+            _previous = null;
+            _variables.Clear();
+
+            ClearSubmissions();
+        }
+
+        [MetaCommand("showTree", "Shows the parse tree")]
+        private void EvaluateShowTree()
+        {
+            _showTree = !_showTree;
+            Console.WriteLine(_showTree ? "Showing parse trees." : "Not showing parse trees.");
+        }
+
+        [MetaCommand("showProgram", "Shows the bound tree")]
+        private void EvaluateShowProgram()
+        {
+            _showProgram = !_showProgram;
+            Console.WriteLine(_showProgram ? "Showing bound trees." : "Not showing bound trees.");
+        }
+
+        [MetaCommand("load", "Loads a script file")]
+        private void EvaluateLoad(string path)
+        {
+            path = Path.GetFullPath(path);
+
+            if (!File.Exists(path))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"error: file does not exist '{path}'");
+                Console.ResetColor();
+
+                return;
+            }
+
+            var text = File.ReadAllText(path);
+
+            EvaluateSubmission(text);
+        }
+
+        [MetaCommand("ls", "Lists all symbols")]
+        private void EvaluateLs()
+        {
+            var compilation = _previous ?? emptyCompilation;
+
+            var symbols = compilation.GetSymbols().OrderBy(s => s.Kind).ThenBy(s => s.Name);
+            foreach (var symbol in symbols)
+            {
+                symbol.WriteTo(Console.Out);
+                Console.WriteLine();
+            }
+        }
+
+        [MetaCommand("dump", "Shows bound tree of a given function")]
+        private void EvaluateDump(string functionName)
+        {
+            var compilation = _previous ?? emptyCompilation;
+
+            var symbol = compilation.GetSymbols().OfType<FunctionSymbol>().SingleOrDefault(f => f.Name == functionName);
+            if (symbol == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"error: function '{functionName}' does not exist");
+                Console.ResetColor();
+
+                return;
+            }
+
+            compilation.EmitTree(symbol, Console.Out);
+        }
+
+        protected override bool IsCompleteSubmission(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return true;
+
+            var lastTwoLineAreBlank = text.Split(Environment.NewLine)
+                                          .Reverse()
+                                          .TakeWhile(s => string.IsNullOrEmpty(s))
+                                          .Take(2)
+                                          .Count() == 2;
+
+            if (lastTwoLineAreBlank)
+                return true;
+
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            // Use Member because we need to exclude the EndOfFileToken.
+            if (syntaxTree.Root.Members.Last().GetLastToken().IsMissing)
+                return false;
+
+            return true;
+        }
+
+        protected override void EvaluateSubmission(string text)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            var compilation = _previous == null
+                ? new Compilation(syntaxTree)
+                : _previous.ContinueWith(syntaxTree);
+
+            if (_showTree)
+            {
+                syntaxTree.Root.WriteTo(Console.Out);
+            }
+
+            if (_showProgram)
+            {
+                compilation.EmitTree(Console.Out);
+            }
+
+            var result = compilation.Evaluate(_variables);
+
+            if (!result.Diagnostics.Any())
+            {
+                if (result.Value != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(result.Value);
+                    Console.ResetColor();
+                }
+
+                _previous = compilation;
+
+                SaveSubmission(text);
+            }
+            else
+            {
+                Console.Out.WriteDiagnostics(result.Diagnostics);
+            }
+        }
+
+        private static string GetSubmissionsDirectory()
+        {
+            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var submissionsDirectory = Path.Combine(localAppData, "Cocoa", "Submissions");
+
+            return submissionsDirectory;
+        }
+
+        private void LoadSubmissions()
+        {
+            var submissionsDirectory = GetSubmissionsDirectory();
+
+            if (!Directory.Exists(submissionsDirectory))
             {
                 return;
             }
 
-			var symbol = _previous.GetSymbols().OfType<FunctionSymbol>().SingleOrDefault(f => f.Name == functionName);
-			if (symbol == null)
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine($"error: function '{functionName}' does not exist");
-				Console.ResetColor();
+            var files = Directory.GetFiles(submissionsDirectory).OrderBy(f => f).ToArray();
 
-				return;
-			}
+            if (files.Length == 0)
+            {
+                return;
+            }
 
-            _previous.EmitTree(symbol, Console.Out);
-		}
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine($"Loaded {files.Length} submission(s)");
+            Console.ResetColor();
 
-		protected override bool IsCompleteSubmission(string text)
-		{
-			if (string.IsNullOrEmpty(text))
-				return true;
+            _loadingSubmission = true;
 
-			var lastTwoLineAreBlank = text.Split(Environment.NewLine)
-										  .Reverse()
-										  .TakeWhile(s => string.IsNullOrEmpty(s))
-										  .Take(2)
-										  .Count() == 2;
+            foreach (var file in files)
+            {
+                var text = File.ReadAllText(file);
 
-			if (lastTwoLineAreBlank)
-				return true;
+                EvaluateSubmission(text);
+            }
 
-			var syntaxTree = SyntaxTree.Parse(text);
+            _loadingSubmission = false;
+        }
 
-			// Use Member because we need to exclude the EndOfFileToken.
-			if (syntaxTree.Root.Members.Last().GetLastToken().IsMissing)
-				return false;
+        private static void ClearSubmissions()
+        {
+            var submissionsDirectory = GetSubmissionsDirectory();
 
-			return true;
-		}
+            if (Directory.Exists(submissionsDirectory))
+            {
+                Directory.Delete(submissionsDirectory, recursive: true);
+            }
+        }
 
-		protected override void EvaluateSubmission(string text)
-		{
-			var syntaxTree = SyntaxTree.Parse(text);
+        private void SaveSubmission(string text)
+        {
+            if (_loadingSubmission)
+            {
+                return;
+            }
 
-			var compilation = _previous == null
-				? new Compilation(syntaxTree)
-				: _previous.ContinueWith(syntaxTree);
+            var submissionsDirectory = GetSubmissionsDirectory();
 
-			if (_showTree)
-			{
-				syntaxTree.Root.WriteTo(Console.Out);
-			}
+            Directory.CreateDirectory(submissionsDirectory);
 
-			if (_showProgram)
-			{
-				compilation.EmitTree(Console.Out);
-			}
+            var count = Directory.GetFiles(submissionsDirectory).Length;
+            var name = $"submission{count:0000}";
+            var filename = Path.Combine(submissionsDirectory, name);
 
-			var result = compilation.Evaluate(_variables);
-
-			if (!result.Diagnostics.Any())
-			{
-				if (result.Value != null)
-				{
-					Console.ForegroundColor = ConsoleColor.White;
-					Console.WriteLine(result.Value);
-					Console.ResetColor();
-				}
-
-				_previous = compilation;
-
-				SaveSubmission(text);
-			}
-			else
-			{
-				Console.Out.WriteDiagnostics(result.Diagnostics);
-			}
-		}
-
-		private static string GetSubmissionsDirectory()
-		{
-			var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-			var submissionsDirectory = Path.Combine(localAppData, "Cocoa", "Submissions");
-
-			return submissionsDirectory;
-		}
-
-		private void LoadSubmissions()
-		{
-			var submissionsDirectory = GetSubmissionsDirectory();
-
-			if (!Directory.Exists(submissionsDirectory))
-			{
-				return;
-			}
-
-			var files = Directory.GetFiles(submissionsDirectory).OrderBy(f => f).ToArray();
-
-			if (files.Length == 0)
-			{
-				return;
-			}
-
-			Console.ForegroundColor = ConsoleColor.DarkGray;
-			Console.WriteLine($"Loaded {files.Length} submission(s)");
-			Console.ResetColor();
-
-			_loadingSubmission = true;
-
-			foreach (var file in files)
-			{
-				var text = File.ReadAllText(file);
-
-				EvaluateSubmission(text);
-			}
-
-			_loadingSubmission = false;
-		}
-
-		private static void ClearSubmissions()
-		{
-			var submissionsDirectory = GetSubmissionsDirectory();
-
-			if (Directory.Exists(submissionsDirectory))
-			{
-				Directory.Delete(submissionsDirectory, recursive: true);
-			}
-		}
-
-		private void SaveSubmission(string text)
-		{
-			if (_loadingSubmission)
-			{
-				return;
-			}
-
-			var submissionsDirectory = GetSubmissionsDirectory();
-
-			Directory.CreateDirectory(submissionsDirectory);
-
-			var count = Directory.GetFiles(submissionsDirectory).Length;
-			var name = $"submission{count:0000}";
-			var filename = Path.Combine(submissionsDirectory, name);
-
-			File.WriteAllText(filename, text);
-		}
-	}
+            File.WriteAllText(filename, text);
+        }
+    }
 }
